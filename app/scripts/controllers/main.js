6578de80dd32f537c998861efb6b259a99e76a44
Benny O <opw0011@gmail.com>
2017-03-23 16:40:53 +0800

    Connect to google book api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the isbnCheckerApp
  */
 angular.module('isbnCheckerApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.book = null;
+
+    $scope.search = function(isbn) {
+      console.log(isbn);
+      $http.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn).then(function(res) {
+        console.log(res.data);
+        $scope.book = res.data;
+      }, function(err) {
+        console.log(err);
+      });
+    }
+
+
   });
