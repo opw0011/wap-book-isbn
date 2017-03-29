5677696e99500001aa745bf6431ba35e7571e08e
Benny O <opw0011@gmail.com>
2017-03-29 12:17:45 +0800

    Populate bookstore data

--- app/scripts/controllers/purchase-refill.js
+++ app/scripts/controllers/purchase-refill.js
@@ -8,7 +8,7 @@
  * Controller of the isbnCheckerApp
  */
 angular.module('isbnCheckerApp')
-  .controller('PurchaseRefillCtrl', function ($scope) {
+  .controller('PurchaseRefillCtrl', function ($scope, $http, $timeout) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -16,6 +16,15 @@ angular.module('isbnCheckerApp')
     ];
 
     $scope.selected = [];
+    $scope.bookstores = [];
+
+    // pagination options
+    $scope.limitOptions = [5, 10, 15, {
+      label: 'All',
+      value: function () {
+        return $scope.bookstores ? $scope.bookstores.count : 0;
+      }
+  }];
     
     $scope.query = {
       order: 'name',
@@ -23,12 +32,22 @@ angular.module('isbnCheckerApp')
       page: 1
     };
     
-    function success(desserts) {
-      $scope.desserts = desserts;
+    function success(bookstores) {
+      $scope.bookstores = bookstores;
     }
     
+    const BASE_URL = "http://127.0.0.1:3000/";
+
     $scope.getDesserts = function () {
+      
       // $scope.promise = $nutrition.desserts.get($scope.query, success).$promise;
     };
+    $http.get(BASE_URL + 'bookstores').then(function(boostores) {
+      console.log(boostores);
+      $scope.bookstores = boostores.data;
+    //   $scope.promise = $timeout(function () {
+    //       $scope.desserts = posts.data;
+    // }, 1000);
+    })
 
   });
