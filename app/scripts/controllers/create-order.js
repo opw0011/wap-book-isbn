2d3d4b749d8dca9938b6fa6811d5373a77ba9582
Benny O <opw0011@gmail.com>
2017-03-30 21:13:33 +0800

    Improve post order grouped by publishers

--- app/scripts/controllers/create-order.js
+++ app/scripts/controllers/create-order.js
@@ -28,40 +28,50 @@ angular.module('isbnCheckerApp')
       // loop through all the books, generate the order grouped by book publishers
       console.log("book order: ", books);
 
-      var orders = [];
+      // group books by publishers
+      // publisher : [books]
+      var orderMap = {};
       for(var book of books) {
         var order = {};
         order.bookid = book.product_id;
-        order.publisher = book.publisher;
         order.quantity = book.orderQuantity;
         order.bookName = book.name;
+
+        if( orderMap[book.publisher]  == null) {
+          orderMap[book.publisher]  = [];
+        }
+
+        orderMap[book.publisher].push(order);
+      }
+
+      // Format the data for POST request
+      var pendingList = [];
+      angular.forEach(orderMap, function(value, key) {
+        console.log(key,  value);
+        var order = {};
+        order.publisher = key;
         order.storeid = book.store_id;
         order.address = $scope.deliveryAddress;
         order.phone = $scope.contactNumber;
         order.createAt = new Date().toISOString();
         order.status = 0; // 0 = pending, 1 = confirmed, 2 = closed
-        orders.push(order);
+        order.books = [];
+        for(book of value) {
+          order.books.push(book);
+        }
+        console.log("Final order: ", order);
+        pendingList.push(order);
+      });
+
+      console.log(pendingList);
+
+      // POST ajax call to server
+      for(var order of pendingList) {
+        $http.post(APP_BASE_URL + 'orders', order).then(function(order) {
+          console.log(order.data);
+        });
       }
-      console.log(orders);
 
-      // var groupedOrders = $filter('groupBy')(orders, 'publisher');
-      // console.log("grouped by: ", groupedOrders);
-
-      // for(var publisher in groupedOrders) {
-      //   console.log(publisher);
-      // }
-
-      // <publisher, [books items]>
-      // angular.forEach(groupedOrders, function(value, key) {
-      //   console.log(key + ': ' + value);
-      // });
-
-      // for(var order of orders) {
-      //   $http.post(APP_BASE_URL + 'orders', order).then(function(order) {
-      //     console.log(order.data);
-      //   });
-      // }
-
-      // $mdDialog.hide();
+      $mdDialog.hide();
     }
   });
