53486e23be45aedcd36c6b3eddb4f93002736459
Benny O <opw0011@gmail.com>
2017-03-31 23:21:59 +0800

    Update new book purchase

--- app/scripts/controllers/purchase-new.js
+++ app/scripts/controllers/purchase-new.js
@@ -8,15 +8,29 @@
  * Controller of the isbnCheckerApp
  */
 angular.module('isbnCheckerApp')
-  .controller('PurchaseNewCtrl', function ($scope, $http, $sce) {
-    $scope.book = null;
+  .controller('PurchaseNewCtrl', function ($scope, $http) {
+
+    // pagination options
+    $scope.limitOptions = [5, 10, 15, {
+      label: 'All',
+      value: function () {
+        return $scope.books ? $scope.books.length : 0;
+      }
+    }];
+
+    $scope.query = {
+      order: '',
+      limit: 5,
+      page: 1,
+      filter: ''
+    };
 
     $scope.search = function(isbn) {
       console.log(isbn);
       var DOUBAN_BOOK_API = 'https://api.douban.com/v2/book/isbn/';
 
       var requestURL = DOUBAN_BOOK_API + isbn;
-      $.ajax({
+      $scope.promise = $.ajax({
           url : requestURL,
           dataType : 'jsonp',
           jsonp: 'callback',
@@ -36,7 +50,7 @@ angular.module('isbnCheckerApp')
 
       var DOUBAN_BOOK_SEARCH_API = 'https://api.douban.com/v2/book/search?q=';
       var requestURL = DOUBAN_BOOK_SEARCH_API + keyword;
-      $.ajax({
+      $scope.promise = $.ajax({
           url : requestURL,
           dataType : 'jsonp',
           jsonp: 'callback',
@@ -49,4 +63,27 @@ angular.module('isbnCheckerApp')
           }   
       }); 
     };
+
+    $scope.orderSelectedItem = function(event) {
+      event.stopPropagation();
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        parent: parentEl,
+        targetEvent: event,
+        templateUrl: 'views/create-order-dialog.html',
+        locals: {
+          bookstore: $scope.bookstore,
+          books: $scope.selectedBooks
+        },
+        controller: 'CreateOrderCtrl'
+      });
+    }
+
+    function init() {
+      $scope.books = [];
+      $scope.selectedBooks = [];
+    }
+
+    init();
+
   });
