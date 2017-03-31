dd3f3e89c6d5ee728f5d23fa6baa6b1995146af3
Benny O <opw0011@gmail.com>
2017-03-31 09:00:03 +0800

    Add jsonp call to Douban book api

--- app/scripts/controllers/purchase-new.js
+++ app/scripts/controllers/purchase-new.js
@@ -8,10 +8,34 @@
  * Controller of the isbnCheckerApp
  */
 angular.module('isbnCheckerApp')
-  .controller('PurchaseNewCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PurchaseNewCtrl', function ($scope, $http, $sce) {
+    $scope.book = null;
+
+    $scope.search = function(isbn) {
+      console.log(isbn);
+      var GOOGLE_BOOK_API = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+      var DOUBAN_BOOK_API = 'https://api.douban.com/v2/book/isbn/';
+      // $http.get(GOOGLE_BOOK_API + isbn).then(function(res) {
+      //   console.log(res.data);
+      //   $scope.book = res.data;
+      // }, function(err) {
+      //   if(err) {
+      //     console.log(err);
+      //     document.alert('book not found');
+      //   }
+      // });
+      var requestURL = DOUBAN_BOOK_API + isbn;
+      $.ajax({
+          url : requestURL,
+          dataType : 'jsonp',
+          jsonp: 'callback',
+          success : function(res){ 
+              // update the $scope variable
+              $scope.$apply(function() {
+                $scope.book = res;
+              });
+              console.log($scope.book);
+          }   
+      }); 
+    };
   });
